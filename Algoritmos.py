afea3dc8be6e1ad746de25e6180bd641ca1f9b36
jadsonmarliere <jds.mdo@gmail.com>
2018-09-27 11:26:33 -0300

    Add BubbleSort
    Organização de código e adicionado Algoritmo de Ordenação BubbleSort

--- Algoritmos.py
+++ Algoritmos.py
@@ -9,3 +9,14 @@ def insertionSort(k):
             j=j-1
         k[j] = temp
     return k
+
+def bubbleSort(lista):
+    elementos = len(lista)-1
+    ordenado = False
+    while not ordenado:
+        ordenado = True
+        for i in range(elementos):
+          if lista[i] > lista[i+1]:
+               lista[i], lista[i+1] = lista[i+1],lista[i]
+               ordenado = False
+    return lista
--- Aplicativo.py
+++ Aplicativo.py
@@ -1,14 +1,36 @@
+#Bibliotecas importadas
 from Listas import *
 from random import *
 from Algoritmos import *
+from time import *
 
 def main():
+
+    #Cria nova lista estabelecida e ordena
     novalista = criaLista1()
     print(sorted(novalista))
 
-    listaFib = listaFibonacci(10)
+    #Cria uma Sequencia de Fibonacci com 'n' posições
+    listaFib = listaFibonacci(10**3)
+
+    #Verifica se a lista está desordenada
     shuffle(listaFib)
     print(listaFib)
+
+    #Ordena e verifica tempo de Execução Insertion Sort
+    inicio = time()
     print(insertionSort(listaFib))
+    fim=time()
+    print('Tempo de execução InsertionSort: %f'  %(fim-inicio))
+
+    #Verifica se a lista está desordenada
+    shuffle(listaFib)
+    print(listaFib)
+
+    #Ordena e verifica tempo de Execução Bubble Sort
+    inicio = time()
+    print(bubbleSort(listaFib))
+    fim = time()
+    print('Tempo de execução BubbleSort: %f' %(fim - inicio))
 
 main()
