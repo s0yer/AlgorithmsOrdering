c13ad39e394db701f0d91c10cf2fde8453acd53e
jadsonmarliere <jds.mdo@gmail.com>
2019-03-26 19:26:07 -0300

    Update Aplicativo.py

--- Aplicativo.py
+++ Aplicativo.py
@@ -1,3 +1,4 @@
+#Libraries imported
 #Bibliotecas importadas
 from Listas import *
 from random import *
@@ -6,27 +7,34 @@ from time import *
 
 def main():
 
+    #Create new established list and sort
     #Cria nova lista estabelecida e ordena
     novalista = criaLista1()
     print(sorted(novalista))
 
+    #Create a Fibonacci Sequence with 'n' positions
     #Cria uma Sequencia de Fibonacci com 'n' posições
     listaFib = listaFibonacci(10**3)
 
-    #Verifica se a lista está desordenada
+    
+    #Shuffles the list -> listaFib
+    #Embaralha a lista -> listaFib
     shuffle(listaFib)
     print(listaFib)
-
+    
+    #Order and check execution time Insertion Sort
     #Ordena e verifica tempo de Execução Insertion Sort
     inicio = time()
     print(insertionSort(listaFib))
     fim=time()
     print('Tempo de execução InsertionSort: %f'  %(fim-inicio))
-
-    #Verifica se a lista está desordenada
+    
+    #Shuffles the list -> listaFib
+    #Embaralha a lista -> listaFib
     shuffle(listaFib)
     print(listaFib)
-
+    
+    #Order and check runtime Bubble Sort
     #Ordena e verifica tempo de Execução Bubble Sort
     inicio = time()
     print(bubbleSort(listaFib))
